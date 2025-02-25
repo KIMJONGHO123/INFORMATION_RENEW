7fc8996877efd0e36302b7b985d3dd6383de5e0a
KIMJONGHO <ho098701@naver.com>
2025-02-25 15:33:57 +0900

    add

--- 정리본/JAVA/MY_JAVABOOK/src/Ch02/C04자료형.java
+++ 정리본/JAVA/MY_JAVABOOK/src/Ch02/C04자료형.java
@@ -37,12 +37,33 @@ public class C04자료형 {
 		
 		// 정수 long -8byte 정수 부호 ㅇ
 		
-		long n1 = 10000000000L;
-		long n2 = 20;  // L,l (리터럴접미사) : long 자료형 사용하여 값 저장
+//		long n1 = 10000000000L;
+//		long n2 = 20;  // L,l (리터럴접미사) : long 자료형 사용하여 값 저장
+//		
+//		long n3 = 100000000000;
+//		long n4 = 100000000000L;
 		
-		long n3 = 100000000000;
-		long n4 = 100000000000L;
-
+		// 실수 
+		
+		// 유리수와 무리수의 통칭
+//		소숫점 이하값을 가지는 수 123.456
+//		float : 4byte 실수 (6-9자리)
+//		double : 8 byte 실수 (15 ~ 18 자리), 기본 자료형
+		
+		// 정밀도 확인
+		float n1 = 0.123456789123456789F;
+		double n2 = 0.123456789123456789;
+		
+		System.out.println(n1);
+		System.out.println(n2);
+		
+		// 오차 확인
+//		float num = 0.1F;
+//		for (int i = 0; i <= 1E5; i++) {
+//			num = num+0.1F;
+//			System.out.println(i);
+//		}
+//		System.out.println("num"+ num);
 	}
 
 }
