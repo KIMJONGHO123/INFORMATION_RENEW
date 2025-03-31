1ad9f339e8a01908f5cdcf51c0f40e1829cb66cc
KIMJONGHO123 <ho098701@naver.com>
2025-03-31 12:28:04 +0900

    add

--- 정리본/DB_ORACLE_MYSQL/ORACLE/LOCALHOST.sql
+++ 정리본/DB_ORACLE_MYSQL/ORACLE/LOCALHOST.sql
@@ -1,9 +1,20 @@
-select * from Reserve_Tbl;
+ALTER SESSION SET tracefile_identifier = '_TEST_';
 
-insert into Member_Tbl values (1234,'김종호','9999','3','대구','아이폰');
-insert into Classification_Tbl values(55556,'박길대');
-insert into Book_Tbl values(1111,55556,'이민하','이것이책인가','한빛출판',1);
-insert into Rental_Tbl values(6363,1111,1234);
-insert into Reserve_Tbl values(6363,1234,'나나나');
-insert into  Appendix_Tbl values(1258,1111,'이름몰라');
+
+-- 추적 시작
+ALTER SESSION SET sql_trace = TRUE;
+
+-- 고급 트레이스 설정: 이벤트 10046 레벨 12는 바인드 변수와 대기 이벤트까지 포함한 상세한 트레이스를 활성화
+ALTER SESSION SET EVENTS '10046 trace name context forever, level 12';
+
+
+-- 실제로 트레이스를 적용할 SQL 문 실행
+select * from usertbl where addr='서울';
+
+
+-- SQL 트레이스 기능 종료: 이후 SQL은 더 이상 트레이스하지 않음
+ALTER SESSION SET sql_trace = FALSE;
+
+-- 트레이스 파일이 저장된 디렉토리 위치 확인
+SELECT VALUE FROM v$diag_info;
 
