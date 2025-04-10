d5a337b9f1b93593bdc98262e4f0ea0019c12114
KIMJONGHO123 <ho098701@naver.com>
2025-04-10 17:35:03 +0900

    add

--- 정리본/DB_ORACLE_MYSQL/ORACLE/LOCALHOST.sql
+++ 정리본/DB_ORACLE_MYSQL/ORACLE/LOCALHOST.sql
@@ -2,3 +2,12 @@ select * from tbl_class_202201;
 select * from tbl_member_202201;
 select * from tbl_teacher_202201;
 commit;
+
+create table login_tbl (
+    id varchar(20) primary key,
+    pw varchar(20),
+    role varchar(20)
+);
+select * from login_tbl;
+
+commit;
